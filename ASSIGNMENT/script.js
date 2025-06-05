f9ea4bbea8aa68d9a5af7d01d9d47e00827ac893
laibabuttjs <laibabut000@gmail.com>
2025-06-05 23:40:13 +0500

    updated

---
+++ ASSIGNMENT/script.js
@@ -0,0 +1,33 @@
+// console
+console.log("My name is Abial");
+console.log(44 - 24);
+console.log(16 + 16);
+console.log(2 * 2);
+console.log(43 - 12);
+console.log(144 + 421);
+console.log(12 * 24);
+console.log(3 + 3 - 2);
+console.log(4 * 8 + 19);
+console.log(7 + 12 * 5 - 22);
+console.log(9 * 9 - 45 * 6);
+console.log(32 - 48 + 44 - 19);
+console.log("Hello person");
+console.log("Weolcome here!");
+console.log("Write whatever you want");
+
+// alert
+// alert("Error 404 (Not found)");
+// alert("Hello World!");
+// alert("Welcome to our site!");
+// alert("Enjoy your stay");
+// alert("This message appears after 3 seconds");
+// alert("Button clicked!");
+// alert("Enter your name:");
+// alert("Do you want to proceed?");
+// alert("You canceled the operation");
+// alert("Welcome, member!");
+// alert("Please sign up to become a member");
+// alert("Log in");
+// alert("Number is wrong");
+// alert("The data you provided does not match this site.");
+// alert("Fixing issues");
