238888ebdbacf34b6ce985359022de4626cb16e0
laibabuttjs <laibabut000@gmail.com>
2025-06-12 17:32:18 +0500

    updated

---
+++ QUIZ/script.js
@@ -0,0 +1,16 @@
+// string , number , float , boolean
+// console.log(abc)
+// var , let , const
+// var fname="Salar";
+// var lname="Butt";
+// console.log(fname + lname);
+var name="Jay"
+var fname="Safdar"
+
+var uni="Iqra"
+var semester="4th"
+// It is certified that Jay son of Safdar is student of Iqra university and currently enrolled in 4th semester
+
+console.log("It is certified that " + name + " son of " + fname + " is student of " + uni + " university and currently enrolled in " + semester + " semester");
+
+console.log(typeof(name));
---
+++ VARIABLE ASSIGNMENT/script.js
@@ -0,0 +1,41 @@
+let x = 10;
+console.log(x);
+
+let message = "Hello World!";
+console.log(message);
+
+const PI = 3.14;
+console.log(PI);
+
+let score = 100;
+score = 200;
+console.log(score);
+
+// const name = "Ali";
+ // name = "Samad"
+// console.log(name);
+
+let a = 5;
+let b = 7;
+let c = a + b; 
+console.log(c);
+
+let name = "Laiba";
+let greeting = "Hello";
+console.log(greeting + " " + name);
+
+let sum ;
+sum = 1 + 2 + 3 + 4 + 5;
+console.log(sum);
+
+let average;
+average = (4 + 6 + 8) / 3;
+console.log(average);
+
+let product;
+product = (6 * 7);
+console.log(product);
+
+let divide; 
+divide = (10 / 3);
+console.log(divide);
