fdcfbeb4210d88649728ca5fb9d7dcc6e2a51dc4
laibabuttjs <laibabut000@gmail.com>
2025-06-20 21:25:27 +0500

    updated

---
+++ COND STATE PR/script.js
@@ -0,0 +1,69 @@
+// let num1 = prompt("Enter number 1");
+// let num2 = prompt("Enter number 2");
+// if(num1 > num2)
+// {
+//     console.log(num1);
+// }
+// else{
+//     console.log(num2);   
+// }
+
+// let num = prompt("Enter any number")
+// if(num > 0)
+// {
+//     console.log("positive");
+    
+// }
+// else{
+//     console.log("negative");
+    
+// }
+
+// let age = prompt("Enter your age")
+// if(age >= 18)
+// {
+//     console.log("You are adult");
+// }   
+
+// else{
+//     console.log("You are minor");
+    
+// }
+
+// let num = prompt("Enter number")
+// if(num %2 === 0){
+//     console.log("Even");
+    
+// }
+// else{
+//     console.log("Odd");
+    
+// }
+
+// let username = prompt("Your Name")
+// if(username === "admin"){
+//     console.log("Welcome Admin!");
+    
+// }
+// else{
+//     console.log("Access denied");
+    
+// }
+
+let marks = prompt("Enter your marks")
+if(marks >= 80 && marks <= 100){
+    console.log("Distinction");
+    
+}
+else if(marks >= 50 && marks < 80){
+    console.log("Pass");
+    
+}
+else if(marks < 50){
+    console.log("Fail");
+    
+}
+else{
+    console.log("Invalid marks");
+    
+}
