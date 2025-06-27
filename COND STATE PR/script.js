2040c18344fcfb7ba576590bb95f193c49453942
laibabuttjs <laibabut000@gmail.com>
2025-06-27 15:20:07 +0500

    updated

--- COND STATE PR/script.js
+++ COND STATE PR/script.js
@@ -50,20 +50,171 @@
     
 // }
 
-let marks = prompt("Enter your marks")
-if(marks >= 80 && marks <= 100){
-    console.log("Distinction");
+// let marks = prompt("Enter your marks")
+// if(marks >= 80 && marks <= 100){
+//     console.log("Distinction");
     
-}
-else if(marks >= 50 && marks < 80){
-    console.log("Pass");
+// }
+// else if(marks >= 50 && marks < 80){
+//     console.log("Pass");
     
-}
-else if(marks < 50){
-    console.log("Fail");
+// }
+// else if(marks < 50){
+//     console.log("Fail");
     
-}
-else{
-    console.log("Invalid marks");
+// }
+// else{
+//     console.log("Invalid marks");
     
-}
+// }
+
+// let vowels = "e";
+
+// if(vowels == "a", "e", "i", "o", "u"){
+//     console.log("vowel");   
+// }
+// else{
+//     console.log("consonant");
+// }
+
+// let a = 12;
+// let b = 7;
+// let c = 19;
+// let smallest;
+
+// if (a <= b && a <= c) {
+//   smallest = a;
+// } else if (b <= a && b <= c) {
+//   smallest = b;
+// } else {
+//   smallest = c;
+// }
+
+// // console.log("Smallest number is:", smallest);
+
+// let number = 64;
+
+// if(number > 10 && number < 50){
+//     console.log("This number is between 10 and 50");
+// }
+// else{
+//     console.log("This number is NOT between 10 and 50");
+// }
+
+// let num = 66;
+
+// if(num % 7 == 0 || num % 11 == 0){
+//     console.log("The number is a multiple of 7 or 11");
+// }
+// else{
+//     console.log("The number is NOT a multiple of 7 or 11");
+// }
+
+// let marks = 70;
+
+// if(marks >= 90 && marks <= 100){
+//     console.log("Grade: A+");
+// }
+// else if(marks >= 80 && marks <= 89){
+//     console.log("Grade: A");   
+// }
+// else if(marks >= 70 && marks <= 79){
+//     console.log("Grade: B");
+// }
+// else if(marks >= 60 && marks <= 69){
+//     console.log("Grade: C");
+// }
+// else if(marks >= 50 && marks <= 69){
+//     console.log("Grade: D");
+// }
+// else{
+//     console.log("Grade: F (Fail) ");
+// }
+
+// let age = 20;
+
+// if(age >= 18){
+//     console.log("Eligible to vote");
+// }
+// else{
+//     console.log("NOT Eligible to vote");
+// }
+
+// let password = "mypass123";
+
+// if(password.length >= 8){
+//     console.log("Password length is valid");
+// }
+// else{
+//     console.log("Password too short (must be at least 8 characters)");
+// }
+
+// let input = prompt("Enter something")
+
+// if(input === ""){
+//     console.log("Input is empty");
+// }
+// else{
+//     console.log(input);
+// }
+
+// let num = 41;
+
+// if(num.length === 2){
+//     console.log("The number has 2 digits");
+// }
+// else if(num.length == 3){
+//     console.log("The number has 3 digits");
+// }
+// else if(num.length == 4){
+//     console.log("The number has 4 digits");
+// }
+// else{
+//     console.log("The number does not have 2, 3, or 4 digits");
+// }
+
+// let str1 = "How are u?"
+// let str2 = "how are u"
+
+// if(str1 === str2){
+//     console.log("The strings are equal");
+// }
+// else{
+//     console.log("The strings are NOT equal");
+// }
+
+// let month = 5;
+
+// if(month == 12 || month == 1 || month == 2){
+//     console.log("Season: Winter");
+// }
+// else if(month >= 3 && month <= 5){
+//     console.log("Season: Spring");
+// }
+// else if(month >= 6 && month <= 8){
+//     console.log("Season: Summer");
+// }
+// else if(month >= 9 && month <= 11){
+//     console.log("Season: Autumn");
+// }
+// else{
+//     console.log("Invalid month number");
+// }
+
+// let num = 114;
+
+// if(num >= 100 && num <= 200){
+//     console.log("The number is within the range of 100-200");
+// }
+// else{
+//     console.log("The number is outside the range");
+// }
+
+// let age = 45;
+
+// if(age >= 60){
+//     console.log("Eligible for senior discount");
+// }
+// else{
+//     console.log("Not eligible for senior discount");
+// }
