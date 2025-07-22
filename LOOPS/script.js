5b9cdf94e51158219c6d42671e7d6116ad69e547
laibabuttjs <laibabut000@gmail.com>
2025-07-23 01:41:04 +0500

    updated

---
+++ LOOPS/script.js
@@ -0,0 +1,35 @@
+// Write a for loop that prints numbers from 1 to 100, but only even numbers.
+// for (let x = 0; x <= 100; x++) {
+//     if(x %2 === 0){
+//         console.log(x);
+//     }
+    
+// }
+
+// Create a loop that prints the multiplication table of 7 up to 10 times (i.e., 7 x 1 to 7 x 10).
+// let num = 7;
+// for (let y = 1; y <= 10; y++) {
+//     console.log(`${num} * ${y} = ${num * y}`); 
+// }
+
+// Use a while loop to sum the numbers from 1 to 50.
+// let n = 1;
+// let z = 1;
+// while (z <= 50 && z >= 0) {
+//     console.log(`${n} + ${z} = ${n + z}`);
+//     z++;
+// }
+
+// Print the square of numbers from 1 to 10 using a for loop.
+// let num = 1;
+// for (let num = 1; num >= 1 && num <= 10; num++) {
+//     console.log(`${num} * ${num} = ${num * num}`);
+// }
+
+// Use a do...while loop to print numbers from 10 to 1 in reverse.
+// let num = 10;
+// do{
+//     console.log(num);
+//     num--;
+// }
+// while(num >= 1);
--- OBJECT/script.js
+++ OBJECT/script.js
@@ -1,64 +1,64 @@
-let student = {
- name : "Ahmed"=
- id : 47928=
- department : "CS"=
- semester : "4th"=
- cgpa : 3.7=
+let student ; {
+ name : "Ahmed";
+ id : 47928;
+ department : "CS";
+ semester : "4th";
+ cgpa : 3.7;
  };
 
  console.log(student.department);
  console.log(student["semester"]);
  
- let car1 = {
-    name : "GLI"=
-    model : 2020=
-    color : "White"=
-    condition : "Good"=
-    price : 3500000=
-    sunRoof : true=    
+ let car1 ; {
+    name : "GLI";
+    model : 2020;
+    color : "White";
+    condition : "Good";
+    price : 3500000;
+    sunRoof : true;    
  };
 
- let car2 = {
-    name : "CROWN"=
-    model : 2015=
-    color : "Black"=
-    condition : "Good"=
-    price : 4500000=
-    sunRoof : false=    
+ let car2 ; {
+    name : "CROWN";
+    model : 2015;
+    color : "Black";
+    condition : "Good";
+    price : 4500000;
+    sunRoof : false;    
  };
 
- let car3 = {
-    name : "MARK X"=
-    model : 2018=
-    color : "Brown"=
-    condition : "excellent"=
-    price : 7000000=
-    sunRoof : true=    
+ let car3 ; {
+    name : "MARK X";
+    model : 2018;
+    color : "Brown";
+    condition : "excellent";
+    price : 7000000;
+    sunRoof : true;    
  };
 
- let car4 = {
-    name : "VITZ"=
-    model : 2013=
-    color : "Silver"=
-    condition : "Good"=
-    price : 6700000=
-    sunRoof : true=    
+ let car4 ; {
+    name : "VITZ";
+    model : 2013;
+    color : "Silver";
+    condition : "Good";
+    price : 6700000;
+    sunRoof : true;    
  };
 
- let car5 = {
-    name : "MEHRAN"=
-    model : 2022=
-    color : "Gray"=
-    condition : "Good"=
-    price : 5500000=
-    sunRoof : false=    
+ let car5 ; {
+    name : "MEHRAN";
+    model : 2022;
+    color : "Gray";
+    condition : "Good";
+    price : 5500000;
+    sunRoof : false    
  };
 
- car1.company = "toyota";
- car2.company = "toyota";
- car3.company = "toyota";
- car4.company = "toyota";
- car5.company = "toyota";
+ car1.company ; "toyota";
+ car2.company ; "toyota";
+ car3.company ; "toyota";
+ car4.company ; "toyota";
+ car5.company ; "toyota";
 
  console.log(car1);
  console.log(car2);
